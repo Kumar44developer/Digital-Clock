3cc3101b750de8949b63e8608636d6ea126bd183
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-12-26 13:04:27 +0530

    Format hours and minutes with leading zero

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ function updateClock(){
     let hours = now.getHours();
     let minutes = now.getMinutes();
     let seconds = now.getSeconds();
+
+    hours = hours < 10 ? "0" + hours : hours;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
